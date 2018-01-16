234db449e81dcca8f065e44214da18f2eaa2f3e9
Oghenetega <okeremeta.tega@gmail.com>
2018-01-16 14:05:11 +0100

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -14,6 +14,26 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    // socket.emit('newEmail', {
+    //     from: 'mike@example.com',
+    //     text: 'Hey good people',
+    //     createdAt: 123
+    // });
+
+    socket.emit('newMessage', {
+        from: 'Johnson',
+        text: 'Hey guys,let us meet',
+        createdAt: 456
+    });
+
+    // socket.on('createEmail', (newEmail) => {
+    //     console.log('createEmail', newEmail);
+    // });
+
+    socket.on('createMessage', (newMessage) => {
+        console.log('createMessage', newMessage);
+    });
+
     socket.on('disconnect', () => {
         console.log('Client disconnected');
     });
