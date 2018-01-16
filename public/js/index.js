7bfd246f47250f9d76d26957c664f88e71784de0
Oghenetega <okeremeta.tega@gmail.com>
2018-01-16 15:00:09 +0100

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,25 +2,11 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    // socket.emit('createEmail', {
-    //     to: 'joe@example.com',
-    //     text: 'Joe how far?'
-    // });
-
-    socket.emit('createMessage', {
-        from: 'Othello',
-        text: 'Arise oh sleeping giants'
-    })
 });
             
 socket.on('disconnect', function () {
     console.log('Disconnected from server');
 });
-
-// socket.on('newEmail', function (email) {
-//     console.log('New email', email);
-// });
 
 socket.on('newMessage', function (message) {
     console.log('Got a new message', message);
--- server/server.js
+++ server/server.js
@@ -14,24 +14,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    // socket.emit('newEmail', {
-    //     from: 'mike@example.com',
-    //     text: 'Hey good people',
-    //     createdAt: 123
-    // });
-
-    socket.emit('newMessage', {
-        from: 'Johnson',
-        text: 'Hey guys,let us meet',
-        createdAt: 456
-    });
-
-    // socket.on('createEmail', (newEmail) => {
-    //     console.log('createEmail', newEmail);
-    // });
-
     socket.on('createMessage', (newMessage) => {
         console.log('createMessage', newMessage);
+
+        io.emit('newMessage', {
+            from: newMessage.from,
+            text: newMessage.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
